efcf35f2ef9a3fefa3af113ea8108dc7f5123f75
Waldo Heudens <waldo@heudens.be>
2021-11-16 21:52:59 +0100

    Model Group is toegevoegd

---
+++ Models/Group.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroupSpace.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Naam")]
+        public string Name { get; set; }
+
+        [Display(Name = "Omschrijving")]
+        public string Description { get; set; }
+
+
+        [Display(Name = "Begonnen")]
+        [DataType (DataType.Date)]
+        public DateTime Started { get; set; }
+
+        [Display(Name = "Beëindigd")]
+        [DataType(DataType.Date)]
+        public DateTime Ended { get; set; }
+    }
+}
